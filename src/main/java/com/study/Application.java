6cb2ae2697dd347f87577e774dc4782f103c005f
jinheebyeon <wlsgml8989@gmail.com>
2021-03-16 23:12:00 +0900

    https://github.com/JinHee8989/30days.git

---
+++ src/main/java/com/study/Application.java
@@ -0,0 +1,14 @@
+package com.study;
+
+import com.study.days.One;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+        One.test1();
+    }
+}
