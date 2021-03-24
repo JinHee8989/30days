ce044c9467064834f4ec37e78e93914693eeb43b
jinheebyeon <wlsgml8989@gmail.com>
2021-03-24 23:21:43 +0900

    # 8일차 문제풀이

--- src/main/java/com/study/days/Eight.java
+++ src/main/java/com/study/days/Eight.java
@@ -1,5 +1,7 @@
 package com.study.days;
 
+import java.util.Scanner;
+
 public class Eight {
 
     public void test(){
@@ -46,6 +48,88 @@ public class Eight {
 //        This student had  scores to average:  and . The student's average grade is . An average grade of  corresponds to the letter grade , so the calculate() method should return the character'O'.
 //
 
+        class Person {
+            protected String firstName;
+            protected String lastName;
+            protected int idNumber;
+
+            // Constructor
+            Person(String firstName, String lastName, int identification){
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.idNumber = identification;
+            }
+
+            // Print person data
+            public void printPerson(){
+                System.out.println(
+                        "Name: " + lastName + ", " + firstName
+                                + 	"\nID: " + idNumber);
+            }
+
+        }
+
+        class Student extends Person{
+
+            private int[] testScores;
+
+
+            Student(String firstName, String lastName, int idNumber, int[] scores){
+
+                super(firstName,lastName,idNumber);
+                this.testScores = scores;
+
+            }
+
+
+            public char calculate(){
+
+                int sum = 0;
+                for(int i = 0; i < testScores.length; i++){
+                    sum += testScores[i];
+                }
+
+                int result = sum/testScores.length;
+
+                if(result>=90){
+                    return 'O';
+                }else if(result >= 80 && result < 90){
+                    return 'E';
+                }else if(result >= 70 && result < 80){
+                    return 'A';
+                }else if(result >= 55 && result < 70){
+                    return 'P';
+                }else if(result >= 40 && result < 55){
+                    return 'D';
+                }else{
+                    return 'T';
+                }
+
+            }
+
+
+        }
+
+
+        class Solution {
+            public void main(String[] args) {
+
+                Scanner scan = new Scanner(System.in);
+                String firstName = scan.next();
+                String lastName = scan.next();
+                int id = scan.nextInt();
+                int numScores = scan.nextInt();
+                int[] testScores = new int[numScores];
+                for(int i = 0; i < numScores; i++){
+                    testScores[i] = scan.nextInt();
+                }
+                scan.close();
+
+                Student s = new Student(firstName, lastName, id, testScores);
+                s.printPerson();
+                System.out.println("Grade: " + s.calculate() );
+            }
+        }
 
 
 
